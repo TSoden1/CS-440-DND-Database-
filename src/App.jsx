bdcc1802a59ca622b7b93b13c0513ba76d6d8e51
Tyler Soden <tbs00012@mix.wvu.edu>
2026-04-14 14:17:02 -0400

    Changed singular design thing i need to commit to test something tho

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import { createBrowserRouter, RouterProvider, useNavigate } from 'react-router-dom'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import './App.css'
-import Login from '../Frontend/Login/Login.jsx'
-import Signup from '../Frontend/Signup/Signup.jsx'
-
 
 
 function App() {
